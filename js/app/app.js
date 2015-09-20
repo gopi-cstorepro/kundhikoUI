9a8e36851a178ae0322bde37b7c9a4d282454c12
umakesan <siva143viji@gmail.com>
2015-09-19 21:40:25 -0500

    app and driver

--- js/app/app.js
+++ js/app/app.js
@@ -1,12 +1,55 @@
-﻿<<<<<<< HEAD
-﻿var strCONST_qcserverURL = 'http://localhost:55286/';
+﻿var strCONST_qcserverURL = 'http://localhost:55922/';
 $(document).ready(function () {
+    $(document).on("click", "#btnCreateUser", function (e) {
+        e.preventDefault();
+        createUser();
+    });
+
+    $(document).on("click", "#btnCreateDriverRoute", function (e) {
+        e.preventDefault();
+        createDriverRoute();
+    });
+
 
 });
 
 
 function createUser() {
+    var formData = $('#frmCreateUser').serialize();
 
+    $.ajax({
+        type: "POST",
+        url: strCONST_qcserverURL + "Resources/handler/createUser.ashx",
+        data: formData,
+        success: function (data) {
+            try {
+                localStorage.myname = "Greg";
+                window.location.href =  'driverinfo.html';
+            }
+            catch (e) {
+
+            }
+        }
+    });
 }
-=======
->>>>>>> origin/master
+
+
+
+function createDriverRoute() {
+    var formData = $('#frmCreateDriverRoute').serialize();
+
+    $.ajax({
+        type: "POST",
+        url: strCONST_qcserverURL + "Resources/handler/createDriverRoute.ashx",
+        data: formData,
+        success: function (data) {
+            try {
+                window.location.href = 'search.html';
+            }
+            catch (e) {
+
+            }
+        }
+    });
+}
+
